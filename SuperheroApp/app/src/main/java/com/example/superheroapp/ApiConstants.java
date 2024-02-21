6d555d5bae1f02f0a18a14d0a4c2a59544f872e8
Daniel Nikolaj Hartwich <73463118+Drenzy@users.noreply.github.com>
2024-02-21 10:58:41 +0100

    Update ApiConstants.java

--- SuperheroApp/app/src/main/java/com/example/superheroapp/ApiConstants.java
+++ SuperheroApp/app/src/main/java/com/example/superheroapp/ApiConstants.java
@@ -1,6 +1,5 @@
 package com.example.superheroapp;
 
 public class ApiConstants {
-    //192.168.0.112
     public static final String BASE_URL = "http://192.168.0.112:8080"; // replace with your local IP
 }
